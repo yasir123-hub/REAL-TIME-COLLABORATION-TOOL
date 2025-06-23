c0a5185f08a82919ab1532d74db411150393b552
yasir123-hub <145029125+yasir123-hub@users.noreply.github.com>
2025-06-23 15:22:59 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('content-change', (data) => {
+    socket.broadcast.emit('content-change', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+server.listen(3000, () => console.log('Server running on http://localhost:3000'));
